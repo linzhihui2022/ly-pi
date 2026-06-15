c827282096084fb855b7b5b3f21850ba51a01818
lychee <lychee@rb2.nl>
2026-06-15 08:40:17 +0800

    chore: remove my-guess-game extension

--- pi-extensions/my-guess-game/scripts/deploy.ts
+++
@@ -1,8 +0,0 @@
-import { mkdir } from "node:fs/promises";
-import { join } from "node:path";
-import { homedir } from "node:os";
-
-const dest = join(homedir(), ".pi/agent/extensions/my-guess-game");
-await mkdir(dest, { recursive: true });
-await Bun.write(join(dest, "index.js"), Bun.file("dist/index.js"));
-await Bun.write(join(dest, "my-guess-game.json"), Bun.file("my-guess-game.json"));
