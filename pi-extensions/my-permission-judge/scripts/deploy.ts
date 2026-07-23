92bc5347aa8f88f5aa11a0d6ad0f615b75cd3de4
lychee <lychee@rb2.nl>
2026-07-23 17:09:15 +0800

    chore(my-permission-judge): remove obsolete files and clean up the extension

--- pi-extensions/my-permission-judge/scripts/deploy.ts
+++
@@ -1,7 +0,0 @@
-import { mkdir } from "node:fs/promises";
-import { homedir } from "node:os";
-import { join } from "node:path";
-
-const dest = join(homedir(), ".pi/agent/extensions/my-permission-judge");
-await mkdir(dest, { recursive: true });
-await Bun.write(join(dest, "index.js"), Bun.file("dist/index.js"));
